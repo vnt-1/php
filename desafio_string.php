6a7ff4bb6273d755dfe6894c4ede36d703a4dacd
vnt-1 <114028830+vnt-1@users.noreply.github.com>
2025-04-10 16:52:10 -0300

    add desafio_string.php

---
+++ desafio_string.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST">
+        <label for="str">Digite uma palavra</label>
+        <input type="text" name="str" required>
+
+        <button type="submit">Enviar</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $str = $_POST["str"];
+
+        $n2 = strlen($str);
+        $n1 = 0;
+
+        if ($n1 < $n2) {
+            for ($i = $n1 + 1; $i < $n2; $i++) {
+                echo "$i ";
+            }
+        } else if ($n2 < $n1) {
+            for ($i = $n2 + 1; $i < $n1; $i++) {
+                echo "$i ";
+            }
+        } else {
+            echo "Não há números intermediários entre dois números iguais.";
+        }
+    }
+    ?>
+</body>
+
+</html>
