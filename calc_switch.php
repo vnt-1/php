e06fbf37a643650eee0fc0ae8aa72241d0d05b0a
vnt-1 <114028830+vnt-1@users.noreply.github.com>
2025-04-10 15:05:26 -0300

    add calc_switch.php

--- calc_switch.php
+++ calc_switch.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Document</title>
 </head>
 
 <body>
-
     <form method="POST">
         <center>
             <h1>Calcular</h1><br>
             <label for="n1">Insira o primeiro número</label>
-            <input type="number" id='n1' name="n1" required>
+            <input type="number" name="n1" required>
 
             <label for="n2">Insira o segundo número</label>
-            <input type="number" id='n2' name="n2" required>
+            <input type="number" name="n2" required>
 
             <button type="submit" name='operacao' value="soma">+</button>
             <button type="submit" name='operacao' value="sub">-</button>
@@ -26,10 +25,10 @@
     </form>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $n1 = $_POST['n1'];
-        $n2 = $_POST['n2'];
-        $op = $_POST['operacao'];
+        $n1 = $_POST["n1"];
+        $n2 = $_POST["n2"];
+        $op = $_POST["operacao"];
+        $resultado = 0;
 
         function soma($n1, $n2)
         {
@@ -45,26 +44,30 @@
             return $n1 * $n2;
         }
 
-        function Div($n1, $n2)
+        function div($n1, $n2)
         {
-            if ($n2 <= 0) {
+            if ($n2 == 0) {
                 return "Não é possivel dividir ";
             }
             return $n1 / $n2;
         }
 
-        if ($op == 'soma') {
-            $resultado = soma($n1, $n2);
-        } else if ($op == 'sub') {
-            $resultado = sub($n1, $n2);
-        } else if ($op == 'mult') {
-            $resultado = mult($n1, $n2);
-        } else if ($op == 'div') {
-            $resultado = div($n1, $n2);
-        } else {
-            $resultado = "operação invalida";
+        switch ($op) {
+            case $op == "soma":
+                $resultado = soma($n1, $n2);
+                break;
+            case $op == "sub":
+                $resultado = sub($n1, $n2);
+                break;
+            case $op == "mult":
+                $resultado = mult($n1, $n2);
+                break;
+            case $op == "div":
+                $resultado = div($n1, $n2);
+                break;
         }
-        echo "<p style='text-align: center'> Resultado da operação: $resultado</p>";
+
+        echo "Resultado da operação: $resultado";
     }
     ?>
 </body>
--- meses.php
+++ meses.php
@@ -17,7 +17,20 @@
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $month = [1 => "Janeiro", 2 => "Fevereiro", 3 => "Março", 4 => "Abril", 5 => "Maio", 6 => "Junho", 7 => "Julho", 8 => "Agosto", 9 => "Setembro", 10 => "Outubro", 11 => "Novembro", 12 => "Dezembro"];
+        $month = [
+            1 => "Janeiro",
+            2 => "Fevereiro",
+            3 => "Março",
+            4 => "Abril",
+            5 => "Maio",
+            6 => "Junho",
+            7 => "Julho",
+            8 => "Agosto",
+            9 => "Setembro",
+            10 => "Outubro",
+            11 => "Novembro",
+            12 => "Dezembro"
+        ];
         $month_number = $_POST["mes"];
 
         if ($month_number > 12 || $month_number < 1) {
